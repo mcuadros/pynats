f05430ec1f975e5ccf6202ce43e324885b750f14
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2014-09-15 23:29:43 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+import re
+from setuptools import setup
+
+
+def get_version_from_init():
+    file = open(os.path.join(os.path.dirname(__file__), 'pynats', '__init__.py'))
+
+    regexp = re.compile(r".*__version__ = '(.*?)'", re.S)
+    version = regexp.match(file.read()).group(1)
+    file.close()
+
+    return version
+
+
+setup(
+    name='persistence',
+    license='MIT',
+    author='Maximo Cuadros',
+    author_email='mcuadros@gmail.com',
+    version=get_version_from_init(),
+    url='https://github.com/mcuadros/pynats',
+    packages=[
+        'pynats'
+    ]
+)
